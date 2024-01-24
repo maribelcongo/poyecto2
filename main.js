172b062df797711efed7951a0a7cb31c2efe03d2
maribelcongo <congoceleste1@gmail.com>
2024-01-24 17:37:12 -0300

    declaracion de constantes de algunos elementos

--- main.js
+++ main.js
@@ -15,3 +15,39 @@
     const btnClaro = document.getElementById("bnt-claro") // btn de claro de contorno (aside text)
     const btnOscuro = document.getElementById("bnt-oscuro") // btn de oscuro de contorno (aside text)
     
+
+             // ELEMENTOS 
+const header = document.querySelector("header") // header
+const web = document.getElementById("contenedor-web") // contenedor web
+const textImg = document.getElementById("text-meme") // etiqueta p que toma el value de input superior
+const img = document.getElementById("div-img") // div contenedor de img
+const textImg2 = document.getElementById("text2-meme") // etiqueta p que toma el value de input inferior 
+const asideImg = document.getElementById("aside-img") // aside img
+const inputImagen = document.getElementById("url") // input de url, para la img (aside img)
+const fondoImg = document.getElementById("fondo-img"); // input tipo color (aside img)
+const spanFondoImg = document.getElementById("span-fondo-img") // span de fondo de imagen (aside img)
+const selectFondo = document.getElementById("select-fondo") // selector de fondo de color (aside img)
+const rangoBrillo = document.getElementById("brillo") // input tipo rango del filtro brillo (aside img)
+const rangoOpacidad = document.getElementById("opacidad") // input tipo rango del filtro opacidad (aside img)
+const rangoContraste = document.getElementById("contraste") // input tipo rango del filtro contraste (aside img)
+const rangoDesenfoque = document.getElementById("desenfoque") // input tipo rango del filtro desenfoque (aside img)
+const escalasDeGrises = document.getElementById("grises") // input tipo rango del filtro escala de grises (aside img)
+const rangoSepia = document.getElementById("sepia") // input tipo rango del filtro sepia (aside img)
+const rangoHue = document.getElementById("hue") // input tipo rango del fritro hue (aside img)
+const rangoSaturno = document.getElementById("saturno") // input tipo rango del fritro saturno (aside img)
+const rangoNegativo = document.getElementById("negativo") // input tipo rango de flitro negativo (aside img)
+const asideText = document.getElementById("aside-text") // aside text
+const inputSuperior = document.getElementById("superior") // input superior que pinta su value en la etiqueta p (aside text)
+const inputInferior = document.getElementById("inferior") // input inferior que pinta su value en la etiqueta p (aside tex)
+const selectText = document.getElementById("fuente-text") // font-family (aside text)
+const fontSize = document.getElementById("font-size") // font-size (aside text)
+const alignCenter = document.getElementById("align-center") // align-center (aside text)
+const alignRight = document.getElementById("align-right") // align-right (aside text)
+const alignLeft = document.getElementById("align-left") // align-left (aside text)
+const inputColor = document.getElementById("color-text-input") // input de color para texto (aside text)
+const inputFondo = document.getElementById("fondo-text-input") // input de fondo para texto (aside text)
+const colorText = document.getElementById("color-text") // span de color de texto (aside text)
+const fondoText = document.getElementById("fondo-text") // span de fondo de texto (aside text)
+const padding = document.getElementById("padding-text") // input de espaciado de texto (aside text)
+const interlineado = document.getElementById("interlineado") // select de interlineado de texto (aside text)
+
