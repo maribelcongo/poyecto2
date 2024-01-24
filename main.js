c3f424b2bdf744852e235d725b549e65cb060efb
maribelcongo <congoceleste1@gmail.com>
2024-01-24 17:33:03 -0300

    en js todos los botones document.getElementById ordenados en html enlace con js en css cambio de algunos nombres

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+    // BOTONES 
+    const btnTexto = document.getElementById("btn-texto") // btn de mostrar aside de text
+    const btnImagen = document.getElementById("btn-imagen") // btn de mostar aside de img
+    const btnToggle = document.getElementById("btn-toggle") //Modo oscuro
+    const btnCancelar = document.getElementById("cancelar") // btn de salir de aside text
+    const btnSalir = document.getElementById("salir") // btn de salir de aside img
+    const btnDescarga = document.getElementById("download-meme-button") // btn de descargar meme
+    const btnRestablecer = document.getElementById("btn-restablecer") // btn de restablecer filtros (aside img)
+    const btnNoTextSup = document.getElementById("sin-text-superior") // btn sin texto superior(aside text)
+    const btnNoTextInf = document.getElementById("sin-text-inferior") // btn sin texto inferior(aside text)
+    const btnSinFondo = document.getElementById("sin-fondo") // input de sin fondo de texto (aside text)
+    const btnNinguno = document.getElementById("bnt-ninguno") // btn de ninguno de contorno (aside text)
+    const btnClaro = document.getElementById("bnt-claro") // btn de claro de contorno (aside text)
+    const btnOscuro = document.getElementById("bnt-oscuro") // btn de oscuro de contorno (aside text)
+    
